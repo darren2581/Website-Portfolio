287c1b6f857830b7a235e1082facdf23b2fee245
darren2581 <darrentanthongen@gmail.com>
2024-11-12 10:21:10 +0800

    Header done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// import logo from './images/DTTE.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <h1>Portfolio</h1>
+        </div>
+        <div>
+          <ul className="navbar">
+            <li className='nav-item'><a href="#">Home</a></li>
+            <li className='nav-item'><a href="#">About</a></li>
+            <li className='nav-item'><a href="#">Skills</a></li>
+            <li className='nav-item'><a href="#">Project</a></li>
+            <li className='nav-item'><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div className='menu'>
+          <button className='menu-btn'>
+            <span class="material-symbols-outlined">menu</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
